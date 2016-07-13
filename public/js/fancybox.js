3d36a9128e5b5609019be49de6952ee7f12ddce7
bgabot89 <bgabot89@gmail.com>
2016-07-13 00:11:09 -0700

    server working and added express

---
+++ public/js/fancybox.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.fancybox').fancybox({
+        openEffect: 'elastic',
+        closeEffect: 'elastic',
+        helpers: {
+          title: {
+              type: 'over'
+            }
+          }
+    });
+});
---
+++ public/js/new.js
@@ -0,0 +1,24 @@
+//Creates an angular module called myapp then proceeds to declare a controller who ties in the data, and the directive which will create the html template for the data.
+var app = angular.module('myapp', []);
+         app.controller('characterController', ['$scope', function($scope) {
+             $scope.characters = [
+                 { name: 'Deemo', info: 'An enigmatic figure who lives in solitude. Proficient in playing the piano.', image: 'images/deemo_icon.png'},
+                 { name: 'The Girl', info: 'A girl who has lost her memories', image: 'images/the_girl_icon.png'}
+               ];
+             $scope.galleries = [
+               { name: 'Daikazoku63'},
+               { name: 'Sishenfan'},
+               { name: 'Sonnyaws'}
+             ];
+            //Gives the show a numeric operator, in this case 0, so when the page renders it will show nothing.
+             $scope.show = 0;
+         }])
+         app.directive('characterHeader', function() {
+             return {
+                 restrict: 'E', // Element directive
+                 scope: {
+                   character: '=characterData'
+                 },
+                 templateUrl: 'data-render.html'
+         };
+       });

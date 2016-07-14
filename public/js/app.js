b7eec54ceb8e6c3c79bedbcad81ef3a7f450b9cf
bgabot89 <bgabot89@gmail.com>
2016-07-14 01:59:07 -0700

    routes working without ejs

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,63 @@
-//Define a module called characters
-angular.module('character-app', []);
+//Creates an angular module called myapp then proceeds to declare a controller who ties in the data, and the directive which will create the html template for the data.
+var app = angular.module('myapp', ['ngRoute']);
+        //configure routes
+        app.config(function($routeProvider, $locationProvider) {
+          $routeProvider
+          //route for the home page
+          .when('/', {
+            templateUrl: '../static/partials/home.html',
+            controller: 'mainController'
+          })
+          //route for the story page
+          .when('/story', {
+            templateUrl: '../static/partials/storyDescription.html',
+            controller: 'storyController'
+          })
+          //route for the character page
+          .when('/characters', {
+            templateUrl: '../static/partials/characters.html',
+            controller: 'characterController'
+          })
+          //route for the fanart page
+          .when('/fanart', {
+            templateUrl: '../static/partials/fanart.html',
+            controller: 'fanartController'
+          })
+          //use the HTML5 History API, will remove # from url
+          $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+          })
+        });
+          //creates a controller named mainController and inject Angular's $scope
+        app.controller('mainController', function($scope) {
+          //creates a message to display in our view
+          $scope.message = 'Index Page';
+        });
+        //creates a controller named storyController and inject Angular's $scope
+        app.controller('storyController', function($scope) {
+          //creates a message to display in our view
+          $scope.message = 'Story!!!';
+        });
+        //creates a controller named characterController and inject Angular's $scope
+        app.controller('characterController', function($scope) {
+          //creates a message to display in our view
+          $scope.characters = [
+                 { name: 'Deemo', info: 'An enigmatic figure who lives in solitude. Proficient in playing the piano.', image: '/static/images/deemo_icon.png'},
+                 { name: 'The Girl', info: 'A girl who has lost her memories', image: '/static/images/the_girl_icon.png'}
+               ];
+        });
+        //creates a controller named fanartController and inject Angular's $scope
+        app.controller('fanartController', function($scope) {
+          $scope.galleries = [
+            { name: 'Daikazoku63', image: '/static/images/deemo_by_daikazoku63.jpg' },
+            { name: 'Sishenfan', image: '/static/images/deemo_by_sishenfan.jpg' },
+            { name: 'Sonnyaws', image: '/static/images/deemo_by_sonnyaws.jpg' }
+          ];
+          $scope.$on('$locationChangeStart',
+            function (event, next, current) {
+              if (next.indexOf('/static/images') > 0) {
+                event.preventDefault();
+              }
+            });
+        });
--- server.js
+++ server.js
@@ -4,16 +4,26 @@ var express = require("express"),
 		path = require('path');
 
 //middleware to allow use of ejs
-app.set("view engine", "ejs");
+// app.set("view engine", "ejs");
 
 //allows for static files i.e... image, css and javascript files
 app.use("/static", express.static("public"));
 
-//get request to server
+//get request to server to render index page
 app.get('/', function(req,res){
-	// console.log(req.body);
-	res.render('index');
+	res.sendfile('./views/index.html');
 });
+
+//get request to server to render story page
+app.get('/story', function(req,res){
+	res.sendfile('./views/story.html');
+});
+
+//get request to server to render characters page
+app.get('/characters', function(req,res){
+	res.sendfile('./views/character.html');
+});
+
 
 //connects server to local port 3000
 app.listen(process.env.PORT  || 3000, function (){

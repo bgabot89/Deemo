2a46966bcef3c367bf7e73670b9a05f37b98887a
bgabot89 <bgabot89@gmail.com>
2016-07-20 15:53:16 -0700

    UI changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 //Creates an angular module called myapp then proceeds to declare a controller who ties in the data, and the directive which will create the html template for the data.
-var app = angular.module('myapp', ['ngRoute', 'youtube-embed']);
+var app = angular.module('myapp', ['ngRoute', 'youtube-embed', 'ngAnimate']);
         //configure routes
         app.config(function($routeProvider, $locationProvider) {
           $routeProvider
@@ -30,36 +30,37 @@ var app = angular.module('myapp', ['ngRoute', 'youtube-embed']);
           })
           //use the HTML5 History API, will remove # from url
           $locationProvider.html5Mode({
-            enabled: true,
-            // requireBase: false
+            enabled: true
           })
         });
           //creates a controller named mainController and inject Angular's $scope
         app.controller('mainController', function($scope) {
           //creates a message to display in our view
-          $scope.message = 'Index Page';
+          $scope.message = 'Welcome to the Deemo Fanpage';
         });
         //creates a controller named storyController and inject Angular's $scope
         app.controller('storyController', function($scope) {
           //creates a message to display in our view
-          $scope.message = 'Story!!!';
+          $scope.message = 'Story';
         });
         //creates a controller named characterController and inject Angular's $scope
         app.controller('characterController', function($scope) {
+          $scope.message = 'Characters';
           //creates a message to display in our view
           $scope.characters = [
-                 { name: 'Deemo', info: 'A silent, enigmatic pianist who lives in solitude. ', image: '/static/images/deemo_icon.png'},
-                 { name: 'The Girl', info: 'A girl who fell from the sky suffering from amnesia.', image: '/static/images/the_girl_icon.png'},
-                 { name: 'The Masked Lady', info: "A masked woman living in Deemo's world. She questions the actions of Deemo and the girl throughout the story.", image: '/static/images/the_maskedlady_icon.png'}
+             { name: 'Deemo', info: 'A silent, enigmatic pianist who lives in solitude. ', image: '/static/images/deemo_icon.png'},
+             { name: 'The Girl', info: 'A girl who fell from the sky suffering from amnesia.', image: '/static/images/the_girl_icon.png'},
+             { name: 'The Masked Lady', info: "A masked woman living in Deemo's world. She questions the actions of Deemo and the girl throughout the story.", image: '/static/images/the_maskedlady_icon.png'}
                ];
         });
         //creates a controller named fanartController and inject Angular's $scope
         app.controller('fanartController', function($scope) {
+          $scope.message = 'Fanart';
           $scope.galleries = [
-            { name: 'Daikazoku63', image: '/static/images/deemo_by_daikazoku63.jpg' },
-            { name: 'Sishenfan', image: '/static/images/deemo_by_sishenfan.jpg' },
-            { name: 'Ayatoki', image: '/static/images/deemo_by_ayatoki.jpg' },
-            { name: 'Sonnyaws', image: '/static/images/deemo_by_sonnyaws.jpg' }
+            { name: 'Daikazoku63', imageURL: '/static/images/deemo_by_daikazoku63.jpg', image:'/static/images/daikazoku63.png' },
+            { name: 'Sishenfan', imageURL: '/static/images/deemo_by_sishenfan.jpg', image:'/static/images/sishenfan.png'},
+            { name: 'Ayatoki', imageURL: '/static/images/deemo_by_ayatoki.jpg', image:'/static/images/ayatoki.png'},
+            { name: 'Sonnyaws', imageURL: '/static/images/deemo_by_sonnyaws.jpg', image:'/static/images/sonnyaws.png'}
           ];
           $scope.$on('$locationChangeStart',
             function (event, next, current) {
@@ -70,6 +71,7 @@ var app = angular.module('myapp', ['ngRoute', 'youtube-embed']);
         });
         //creates controller for youtube videos
         app.controller('youtubeController', function($scope) {
+          $scope.message = 'Music Samples';
           //video ids for each
           var first = 'BFdhd8lly58';
           var second = 'dWR9Fmypx4w';
